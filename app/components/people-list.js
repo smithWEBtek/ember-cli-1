2c50489e63bc4937827797e1a71334587d790eeb
smithwebtek <brad@smithwebtek.com>
2019-09-15 10:19:14 -0400

    Add showPerson function to people-list component

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    showPerson(person) {
+      // alert(person.name)
+      console.log('you clicked: ', person.name)
+    }
+  }
 });
